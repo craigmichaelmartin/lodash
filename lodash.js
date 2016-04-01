06adab9a745c9c5951d3e6b5ae9e32b4e8df1a5e
Craig Martin <cmartin@onshift.com>
2016-04-01 10:14:54 -0400

    blah

--- lodash.js
+++ lodash.js
@@ -11,7 +11,7 @@
   /** Used as a safe reference for `undefined` in pre-ES5 environments. */
   var undefined;
     
-    var pig = 'asdf'//sdfsda
+    var pig = 'asdf'//sdfsdad
 
   /** Used as the semantic version number. */
   var VERSION = '4.7.0';
